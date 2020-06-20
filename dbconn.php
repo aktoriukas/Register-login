20984f38e3705a7bbca70ae6c73a218049b74530
Gediminas Strumila <63856453+aktoriukas@users.noreply.github.com>
2020-06-20 21:50:09 +0100

    v3.0

--- dbconn.php
+++
@@ -1,8 +0,0 @@
-<?php
-        $servername = "localhost";
-        $username = "root";
-        $password = "aktoriukaz1";
-        $dbname = "users";
-
-        $conn = new mysqli($servername, $username, $password, $dbname);
-?> 
